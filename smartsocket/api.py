24776286a821649e63266fca0070cab606e598d5
xiaolu <183138896@qq.com>
2020-01-04 11:53:56 +0800

    添加设备工作状态提示

--- smartsocket/api.py
+++ smartsocket/api.py
@@ -7,30 +7,57 @@ import paho.mqtt.client as mqtt
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
 
-
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
+    print('初始设备信息')
+    statu_dict = eval(eval(str(msg.payload)[1::]))
+    socket1 = Socket.objects.get(id=1)
+    socket2 = Socket.objects.get(id=2)
+    socket1.status = statu_dict['statu1']
+    socket2.status = statu_dict['statu2']
+    if statu_dict['statu1'] == 'true':
+        socket1.msg = '通电'
+    else:
+        socket1.msg = '断电'
+    if statu_dict['statu2'] == 'true':
+        socket2.msg = '通电'
+    else:
+        socket2.msg = '断电'
+    socket1.save()
+    socket2.save()
+    client.disconnect()
 
+# def on_subscribe(client, userdata):
+#     print('已订阅到消息')
+#
 
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
 def socket_data(request):
+    client = mqtt.Client()
+    try:
+        client.connect("114.55.33.165", 1883, 60)
+        connect_result = client.connect("114.55.33.165", 1883, 60)
+    except OSError:
+        connect_result = True
+    if not bool(connect_result):
+        client.subscribe("socket_status")
+        client.on_message = on_message
+        # client.on_subscribe = on_subscribe
+        client.loop_forever()
+    print('数据更新成功')
     socket = Socket.objects.all()
     socketSer = SocketSerializers(socket, many=True)
-    jasonData = {'socketData': socketSer.data}
+    jasonData = {'socketData': socketSer.data, 'esp8266_run': 'true'}
     return Response(jasonData)
 
-@api_view(['GET', 'POST'])
+@api_view(['POST'])
 def socketSwitch1(request):
     data_dict = request.POST
     for key, val in data_dict.items():
         statu = (key + val)
     client = mqtt.Client()
-    # client.on_connect = on_connect
-    # client.on_message = on_message
-    client.connect("127.0.0.1", 1883, 60)
-    # 发布 test主题
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect("114.55.33.165", 1883, 60)
     client.publish("switch", statu)
-    # print(statu)
+    print(statu)
     return Response(None)
-
-
